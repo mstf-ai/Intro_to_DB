4dd4571106b58886e17e92d0c6b60dec955b4ee3
mstf-ai <albrnsmstfy252@gmail.com>
2025-10-29 14:26:04 +0300

    M

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Select the correct database
+USE alx_book_store;
+
+-- Show full description of the books table without using DESCRIBE or EXPLAIN
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
